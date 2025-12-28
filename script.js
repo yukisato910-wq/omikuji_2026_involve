718df3457a6625338d9d95d0e65a938783dc650d
yukisato910-wq <yuki.sato910@gmail.com>
2025-12-28 09:47:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const cards = [
-    { name: "daikichi", imageUrl: "img/daikichi.png" },
-    { name: "chukichi", imageUrl: "img/chukichi.png" },
-    { name: "chukichi", imageUrl: "img/chukichi.png" },
-    { name: "chukichi", imageUrl: "img/chukichi.png" },
-    { name: "chukichi", imageUrl: "img/chukichi.png" },
-    { name: "shokichi", imageUrl: "img/shokichi.png" },
-    { name: "shokichi", imageUrl: "img/shokichi.png" },
-    { name: "shokichi", imageUrl: "img/shokichi.png" },
-    { name: "shokichi", imageUrl: "img/shokichi.png" },
-    { name: "kyou", imageUrl: "img/kyou.png" },
+    { name: "10", imageUrl: "img/10.png" },
+    { name: "11", imageUrl: "img/11.png" },
+    { name: "12", imageUrl: "img/12.png" },
+    { name: "13", imageUrl: "img/13.png" },
+    { name: "8", imageUrl: "img/8.png" },
+    { name: "9", imageUrl: "img/9.png" },
     // その他のカードを追加
 ];
 
